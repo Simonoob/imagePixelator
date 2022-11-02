a9e9685450e65ea9b56e9da1bde14a4979cb7c03
simonoob <simonefiorentini1@gmail.com>
2022-11-02 17:21:03 +0200

    add analytics support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.scss'
 import type { AppType } from 'next/dist/shared/lib/utils'
+import { usePanelbear } from '@panelbear/panelbear-nextjs'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+	usePanelbear('5LhAmT00vw7')
 	return <Component {...pageProps} />
 }
 
